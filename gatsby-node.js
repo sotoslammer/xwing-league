b472116d1373962c231375e256ff1bc735de532f
sotoslammer <desjarlais.brendan@gmail.com>
2020-04-07 15:01:48 -0600

    get the google sheets api working

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,21 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require("./credentials.json");
 
-// You can delete this file if you're not using it
+const spreadSheetId = "1b5QNn5A8Y5YwFCV3Pc5x9TuLqibgcbp-N_bDUiizvCE"
+
+exports.onPostBootstrap = async () => {
+  try {
+    const doc = new GoogleSpreadsheet(spreadSheetId);
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo(true);
+    // const rows = res.data.values;
+    console.log(doc)
+    const playerSheet = doc.sheetsByIndex[0]
+    await playerSheet.loadHeaderRow();
+    console.log(playerSheet.headerValues);
+    const rows = await playerSheet.getRows();
+    console.log(rows);
+  } catch (err) {
+    return console.log("The API returned an error: " + err)
+  }
+}
